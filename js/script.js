1651d316bbc80c40cbb2a3ecb4d1b2770601dcbf
Joan <kkasemiire711@gmail.com>
2024-01-23 13:10:25 +0300

    Added button click event

--- js/script.js
+++ js/script.js
@@ -4,5 +4,6 @@ var cat = document.querySelector(".cat")
 console.log("cat");
 
 button.addEventListener("click", function(){
-  console.log("Its working!");
+  // console.log("Its working!");
+  cat.classList.add("show")
 })
